72d10f9833e83cfa350f3413d7d67731b19d8361
c43721 <rlahman.game@gmail.com>
2021-02-14 22:13:08 -0600

    Python: Changes to BFS to include edge cases

--- python/search/bfs.py
+++ python/search/bfs.py
@@ -6,9 +6,7 @@ def bfs(visited, graph, start, target):
     queue.append(start)
 
     while queue:
-        cur = queue.pop()
-        print(cur)
-
+        cur = queue.pop(0)
         if cur == target:
             break
 
@@ -16,21 +14,30 @@ def bfs(visited, graph, start, target):
             if child not in visited:
                 visited.append(child)
                 queue.append(child)
+    else:
+         return None
 
     return visited
 
 if __name__ == "__main__":
     #       A
     #     B   C
-    #   D  E    F
+    #   D  E F  G
     graph = {
         'A' : ['B','C'],
         'B' : ['D', 'E'],
-        'C' : ['F'],
+        'C' : ['F', 'G'],
         'D' : [],
         'E' : ['F'],
-        'F' : []
+        'F' : [],
+        'G' : []
     }
 
-    res = bfs(visited, graph, "A", "E")
-    print(res)
+    find = "Q"
+    start = "A"
+    res = bfs(visited, graph, start, find)
+    if res == None:
+        print(f"Can't find node {find} in graph...")
+    else:
+        print(f"Found node {find}, visited: {res}")
+        
