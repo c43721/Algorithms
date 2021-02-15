cdce82269c0a9f2d757a35e454f7ca6510105627
c43721 <rlahman.game@gmail.com>
2021-02-15 16:45:39 -0600

    Python: work on mergesort, not finished.

--- python/sorting/mergesort.py
+++ python/sorting/mergesort.py
@@ -2,8 +2,23 @@ def merge(items):
     # Base case
     if len(items) <= 1:
         return items
+    
+    # Declare sub lists left and right to call recursive sort
+    left = []
+    right = []
+
+    for i in items:
+        left.append(i) if i < len(items) / 2 else right.append(i)
+    
+
+    sorted_left = merge(left)
+    sorted_right = merge(right)
+    # the final merge, merge the sorted left and right, and call merge on that...
+    return sorted_left + sorted_right
 
 if __name__ == "__main__":
-    assert(merge([1,5,3,4,2]), [1, 2, 3, 4, 5])
+    # print(merge([1,5,3,4,2]))
+    print(merge([1,5,3,4,2,6,9,8,7,10]))
+    # assert merge([1,5,3,4,2]) == [1, 2, 3, 4, 5]
 
-    return True
+    print(True)
