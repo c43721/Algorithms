ca5296c962db9a257cc97bde187b1faf2b42b153
c43721 <rlahman.game@gmail.com>
2021-02-16 15:45:01 -0600

    Python: complete mergesort code

--- python/sorting/mergesort.py
+++ python/sorting/mergesort.py
@@ -1,24 +1,42 @@
-def merge(items):
+def merge_sort(items):
     # Base case
     if len(items) <= 1:
         return items
-    
-    # Declare sub lists left and right to call recursive sort
-    left = []
-    right = []
 
-    for i in items:
-        left.append(i) if i < len(items) / 2 else right.append(i)
-    
+    mid = len(items)//2
 
-    sorted_left = merge(left)
-    sorted_right = merge(right)
-    # the final merge, merge the sorted left and right, and call merge on that...
-    return sorted_left + sorted_right
+    l = items[:mid]
+    r = items[mid:]
 
+    merge_sort(l)
+    merge_sort(r)
+
+    i = 0
+    j = 0
+    k = 0
+
+    while i < len(l) and j < len(r):
+        if l[i] < r[j]:
+            items[k] = l[i]
+            i += 1
+        else:
+            items[k] = r[j]
+            j += 1
+        k += 1
+
+    while i < len(l):
+        items[k] = l[i]
+        i += 1
+        k += 1
+
+    while j < len(r):
+        items[k] = r[j]
+        j += 1
+        k += 1
+
+    return items
 if __name__ == "__main__":
     # print(merge([1,5,3,4,2]))
-    print(merge([1,5,3,4,2,6,9,8,7,10]))
+    print(merge_sort([1,5,3,4,2,6,9,8,7,10]))
+    print(merge_sort([3,2,1]))
     # assert merge([1,5,3,4,2]) == [1, 2, 3, 4, 5]
-
-    print(True)
