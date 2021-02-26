8239704765836c7508ce2956bd6948c3496cf80a
c43721 <rlahman.game@gmail.com>
2021-02-26 16:09:33 -0600

    Python: re-do quicksort, using recursion and iteration

--- python/sorting/quicksort.py
+++ python/sorting/quicksort.py
@@ -1,3 +1,30 @@
+def swap(array, i, j):
+	array[i], array[j] = array[j], array[i]
+
+def helper(array, start, end):
+	if start >= end:
+		return
+	pivot = start
+	left = start+1
+	right = end
+	while right >= left:
+		if array[left] > array[pivot] and array[right] < array[pivot]:
+			swap(array, left, right)
+		if array[left] <= array[pivot]:
+			left+=1
+		if array[right] >= array[pivot]:
+			right-=1
+	swap(array, pivot, right)
+
+    # Optimize to get nlogn time
+	leftIsSmaller = right - 1 - start < end - (right+1)
+	if leftIsSmaller:
+		helper(array, start, right-1)
+		helper(array, right+1, end)
+	else:
+		helper(array, right+1, end)
+		helper(array, start, right-1)
+
 def quick_sort(items):
     """Quick sort in Python
 
@@ -10,14 +37,8 @@ def quick_sort(items):
     >>> quick_sort([6,1,4,2,3,5])
     [1, 2, 3, 4, 5, 6]
     """
-    if len(items) <= 1:
-        return items
-
-    pivot = items.pop()
-    low = [low for low in items if low < pivot]
-    high = [high for high in items if high >= pivot]
-
-    return quick_sort(low) + [pivot] + quick_sort(high)
+    helper(items, 0, len(items)-1)
+    return items
 
 if __name__ == "__main__":
     import doctest
